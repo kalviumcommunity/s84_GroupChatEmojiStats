c3a9c88028a6f7b7ab3261299d5ebe11c4213eea
aryaman22-hubb <aryamanpanwar187@gmail.com>
2025-03-20 09:56:28 +0530

    Error fixed

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const emojiRoutes = require('./emojiroutes'); // Replace with your actual routes file
+const emojiRoutes = require('./routes'); // Replace with your actual routes file
 const cors = require('cors');
 
 const app = express();
